308be56d0fb243f21e9e2c10b891a8e0bcc3b929
Zoltán Zemko <zemko_z@hotmail.com>
2021-02-09 20:18:14 +0100

    Optimize Modal dialog for render it's contain only when modal is visible.

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Backdrop from '../Backdrop/Backdrop'
 import classes from './Modal.css';
 
-const modal = (props) => (
-    <React.Fragment>
-        <Backdrop show={props.show} clicked={props.modalClosed} />
-        <div className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}
-        >
-            {props.children}
-        </div>
-    </React.Fragment>
-);
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        return nextProps.show !== this.props.show;
+    }
 
-export default modal;
+    render() {
+        return (
+            <React.Fragment>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div className={classes.Modal}
+                     style={{
+                         transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                         opacity: this.props.show ? '1' : '0'
+                     }}
+                >
+                    {this.props.children}
+                </div>
+            </React.Fragment>);
+    }
+}
+
+export default Modal;
